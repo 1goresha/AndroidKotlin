dc0580c608ca4e7553a0e8ced84cf7deef60c21e
1goresha <i.deryabin2017@mail.ru>
2020-04-09 16:33:02 +0300

    add Nullable project

---
+++ Nullable/app/src/main/java/com/example/nullable/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.nullable
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intentForSecondActivity = Intent(this, SecondActivity::class.java)
+
+        button.setOnClickListener {
+            textView.text = editText.text
+            intentForSecondActivity.putExtra("TEXT", textView.text)
+            startActivity(intentForSecondActivity)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+
+        }
+        super.onSaveInstanceState(outState)
+    }
+}
