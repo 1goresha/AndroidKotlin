f02786595b46aabedcd777d18251fec8df6f9e94
1goresha <i.deryabin2017@mail.ru>
2020-04-15 08:06:31 +0300

    bla bla

--- Nullable/app/src/main/java/com/example/nullable/SecondActivity.kt
+++ Nullable/app/src/main/java/com/example/nullable/SecondActivity.kt
@@ -2,14 +2,15 @@ package com.example.nullable
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
     var count = 0
-    var string : String? = null
-
+    var string: String? = null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -24,14 +25,16 @@ class SecondActivity : AppCompatActivity() {
 
         button3.setOnClickListener {
             string = editText2.text.toString()
-            textView3.text  = string
+            textView3.text = string
         }
+
+        button3.setOnClickListener(object : View.OnClickListener)
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-            outState.putInt("count", count)
-            outState.putString("string", string)
+        outState.putInt("count", count)
+        outState.putString("string", string)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
