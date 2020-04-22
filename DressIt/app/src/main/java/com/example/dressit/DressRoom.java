4178b03e43fdd2a536e2ba192c18fdb19ed5eb89
1goresha <i.deryabin2017@mail.com>
2020-04-22 23:20:17 +0300

    update 22.04.2020 night

--- DressIt/app/src/main/java/com/example/dressit/DressRoom.java
+++ DressIt/app/src/main/java/com/example/dressit/DressRoom.java
@@ -4,21 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class DressRoom extends AppCompatActivity {
 
     private TextView textView;
+    private ArrayList<Integer> hats;
+    private Integer currentCountOfHat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dress_room);
 
+        currentCountOfHat = 0;
+        hats = new ArrayList<>();
+        hats.add(R.drawable.hat1);
+        hats.add(R.drawable.hat2);
+        hats.add(R.drawable.hat3);
         textView = findViewById(R.id.userNameTextView);
         Intent intent = getIntent();
         String userName = intent.getStringExtra("userName");
 
         setTitle(userName + " : Room");
+    }
+
+    public void doNextHat(View view) {
+        if (currentCountOfHat > hats.size() - 1) {
+            currentCountOfHat = 0;
+        }
+        ImageView hat = findViewById(R.id.hat);
+        hat.setImageResource(hats.get(currentCountOfHat));
+        currentCountOfHat++;
+    }
+
+    public void doPreviousHat(View view) {
+        if (currentCountOfHat < 0) {
+            currentCountOfHat = hats.size() - 1;
+        }
+        ImageView hat = findViewById(R.id.hat);
+        hat.setImageResource(hats.get(currentCountOfHat));
+        currentCountOfHat--;
     }
 }
